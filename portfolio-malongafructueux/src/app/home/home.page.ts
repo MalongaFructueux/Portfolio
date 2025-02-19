6e55387d1f3807f3b974cc25dc5bf02fd47eb49c
Malonga Fructueux <malongafructueux48@gmail.com>
2025-02-19 03:10:31 +0100

    petite mise a jou de nuit 3h10 du matin le 19/02/2025

--- portfolio-malongafructueux/src/app/home/home.page.ts
+++ portfolio-malongafructueux/src/app/home/home.page.ts
@@ -87,7 +87,7 @@ export class HomePage {
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      element.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
